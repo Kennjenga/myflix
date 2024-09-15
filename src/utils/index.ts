9bc75ca929c687b7937d1ed4ef00a585821a7c5d
Kenneth Njenga <kinyagia10@gmail.com>
2024-09-15 05:18:00 +0300

    config url

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 export const getCanonicalUrl = () => {
     return process.env.NODE_ENV !== 'production'
       ? 'http://localhost:3000'
-      : 'https://myflix-gilt.vercel.app';
+      : 'https://myflix-gilt.vercel.app/';
   };
   
