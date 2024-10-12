121d8eb1ee221ec5d969fc57eb44c8a29e58fa5e
kennjenga <kinyagia10@gmail.com>
2024-10-12 18:42:02 +0300

    Refactor getCanonicalUrl to support different deployment environments

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,12 @@
 export const getCanonicalUrl = () => {
-    return process.env.NODE_ENV !== 'production'
-      ? 'http://localhost:3000'
-      : 'myflix-amber.vercel.app';
-  };
-  
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    // We're on a Vercel deployment (production or preview)
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  } else if (process.env.NEXT_PUBLIC_SITE_URL) {
+    // We're on a custom domain or another hosting provider
+    return process.env.NEXT_PUBLIC_SITE_URL;
+  } else {
+    // Fallback for local development
+    return 'http://localhost:3000';
+  }
+};
